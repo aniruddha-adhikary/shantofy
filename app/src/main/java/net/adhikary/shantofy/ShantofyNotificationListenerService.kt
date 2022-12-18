be8372114c4479ae293b66ca7c3ba1f4027eef42
Aniruddha Adhikary <aniruddha@adhikary.net>
2022-12-18 19:24:35 +0800

    fix: make service sticky

--- app/src/main/java/net/adhikary/shantofy/ShantofyNotificationListenerService.kt
+++ app/src/main/java/net/adhikary/shantofy/ShantofyNotificationListenerService.kt
@@ -1,5 +1,6 @@
 package net.adhikary.shantofy
 
+import android.content.Intent
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
@@ -10,6 +11,10 @@ import net.adhikary.shantofy.core.ShantofyNotification
 class ShantofyNotificationListenerService : NotificationListenerService() {
 
     private val ruleBasedNotificationChecker = RuleBasedNotificationChecker();
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return START_STICKY;
+    }
 
     override fun onListenerConnected() {
         Log.e(null, "onListenerConnected")
