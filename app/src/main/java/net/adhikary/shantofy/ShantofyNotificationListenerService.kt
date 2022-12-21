111a1712e24cbcfbb6284fdd85cf799c02f462f7
Aniruddha Adhikary <aniruddha@adhikary.net>
2022-12-21 11:48:30 +0800

    fix: mute foreground notification audio

--- app/src/main/java/net/adhikary/shantofy/ShantofyNotificationListenerService.kt
+++ app/src/main/java/net/adhikary/shantofy/ShantofyNotificationListenerService.kt
@@ -5,11 +5,14 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_MUTABLE
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.provider.Settings
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
+import android.text.TextUtils
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import net.adhikary.shantofy.core.RuleBasedNotificationChecker
@@ -18,10 +21,11 @@ import net.adhikary.shantofy.core.ShantofyNotification
 class ShantofyNotificationListenerService : NotificationListenerService() {
 
     private val ruleBasedNotificationChecker = RuleBasedNotificationChecker();
+    private val enabledNotificationListeners = "enabled_notification_listeners"
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         registerNotificationChannel()
-        val notification = createNotification(getCount())
+        val notification = createNotification()
         startForeground(NOTIFICATION_CHANNEL_ID, notification)
         return START_NOT_STICKY
     }
@@ -52,14 +56,21 @@ class ShantofyNotificationListenerService : NotificationListenerService() {
             NotificationManager.IMPORTANCE_DEFAULT
         )
 
+        channel.setSound(null, null)
+
         val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         manager.createNotificationChannel(channel)
     }
 
-    private fun createNotification(count: Int): Notification {
+    private fun createNotification(): Notification {
         val notificationIntent = Intent(this, MainActivity::class.java)
         val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, FLAG_MUTABLE)
-        return buildNotReadyContent()
+        val content = if (!isNotificationServiceEnabled()) {
+            buildNotReadyContent()
+        } else {
+            buildNotificationContent(getCount())
+        }
+        return content
             .setContentIntent(pendingIntent)
             .build()
     }
@@ -72,13 +83,13 @@ class ShantofyNotificationListenerService : NotificationListenerService() {
     }
 
     private fun buildNotReadyContent() =
-        NotificationCompat.Builder(this, getString(R.string.notification_channel_id))
+        Notification.Builder(this, getString(R.string.notification_channel_id))
             .setContentTitle(getString(R.string.notification_listener_service))
             .setContentText(getString(R.string.notification_listener_service_not_ready))
             .setSmallIcon(R.drawable.ic_notification_icon)
 
     private fun buildNotificationContent(count: Int) =
-        NotificationCompat.Builder(this, getString(R.string.notification_channel_id))
+        Notification.Builder(this, getString(R.string.notification_channel_id))
             .setContentTitle(getString(R.string.notification_listener_service))
             .setContentText(getString(R.string.notification_listener_service_content, count))
             .setSmallIcon(R.drawable.ic_notification_icon)
@@ -96,6 +107,25 @@ class ShantofyNotificationListenerService : NotificationListenerService() {
         val prefs: SharedPreferences =
             getSharedPreferences(getString(R.string.preference_key), MODE_PRIVATE)
         return prefs.getInt(getString(R.string.block_count_key), 0)
+    }
+
+    private fun isNotificationServiceEnabled(): Boolean {
+        val flat: String = Settings.Secure.getString(
+            contentResolver,
+            enabledNotificationListeners
+        )
+        if (!TextUtils.isEmpty(flat)) {
+            val names = flat.split(":").toTypedArray()
+            for (i in names.indices) {
+                val cn = ComponentName.unflattenFromString(names[i])
+                if (cn != null) {
+                    if (TextUtils.equals(packageName, cn.packageName)) {
+                        return true
+                    }
+                }
+            }
+        }
+        return false
     }
 
     companion object {
