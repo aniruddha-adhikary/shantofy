63e9e95e673d6d5c5ff4882ecfeee6dbaabe5536
Aniruddha Adhikary <aniruddha@adhikary.net>
2022-12-21 12:36:35 +0800

    feat: block some more apps

--- app/src/main/java/net/adhikary/shantofy/core/TargetApps.kt
+++ app/src/main/java/net/adhikary/shantofy/core/TargetApps.kt
@@ -14,12 +14,28 @@ enum class TargetApps(val packageName: String, val blockChannels: Set<String>? =
     EasternBankEBL("com.cibl.eblsky"),
     MutualTrustBankMTB("com.mtb.mobilebanking"),
     TheCityBank("com.thecitybank.citytouch"),
+    BracBank("com.bracbank.astha"),
+    FSIB("com.fsiblbd.fsiblcloud"),
+    PrimeBank("bd.com.primebank.pib.altitudemobile"),
+    DhakaBank("com.gplex.dhakabank"),
+    SonaliBank("bd.com.sonalibank.sonalie_sheba"),
+    Bengal("com.bengal.newdigitalbanking"),
+    TrustBank("com.cibl.tbl"),
+    NRBBank("com.sslwireless.nrbmobapp"),
+    AIBL("com.bd.aibl.ibapps"),
 
     // mfs
     Bkash("com.bKash.customerapp"),
     Nagad("com.konasl.nagad", setOf("ANNOUNCEMENT")),
+    Rocket("com.dbbl.mbs.apps.main"),
+    Upay("bd.com.upay.customer"),
+    Tap("com.tad.bdkepler"),
 
     // telco
+    Teletalk("teletalk.teletalkcustomerapp"),
+    Airtel("net.omobio.airtelsc"),
+    Banglalink("com.arena.banglalinkmela.app"),
+    BanglalinkLite("net.banglalink.mybllite"),
     MyGP("com.portonics.mygp");
 
     companion object {
